eb76e8b59bc518fa00e7f13509693f5258358d66
Gustavo Graciano <gustavo.silva123@aluno.senai.br>
2023-07-28 11:06:18 -0300

    Entendendo os Wrappers

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -21,7 +21,11 @@ public class Fundamentos {
         double media = 5.5;
         
         String bairro = new String("Centro");
-                
+        
+        //Declaração de Wrappers
+        int nota1 = Integer.parseInt("7");
+        double mediaF = Double.parseDouble("7.5");
+        boolean aprovado = Boolean.parseBoolean("true");
     }
     
 }
