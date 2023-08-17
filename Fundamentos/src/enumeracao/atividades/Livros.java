c48dc29bdbc7cb0683e0721f88b7f7f778baf8e9
Gustavo Graciano <gustavosilva@aluno.senai.br>
2023-08-17 10:50:48 -0300

    Enviando atividade de Enumeração em Java

---
+++ Fundamentos/src/enumeracao/atividades/Livros.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao.atividades;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Livros {
+    public enum enumLivros{
+        ROMANCE, ACAO, AVENTURA, FANTASIA, SUSPENSE, TERROR
+    }
+}
---
+++ Fundamentos/src/enumeracao/atividades/MesesAno.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enumeracao.atividades;
+
+import javax.management.MBeanInfo;
+
+/**
+ *
+ * @author Aluno
+ */
+public class MesesAno {
+    public enum enumMeses{
+       JANEIRO, FEVEREIRO, MARCO, ABRIL, MBeanInfo, JUNHO, JULHO, AGOSTO, SETEMBRO, OUTUBRO, NOVEMBRO, DEZEMBRO
+    }
+}
